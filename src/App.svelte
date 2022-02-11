aa836cbd6ba7bad7ae0a97e1320f35a1acc30504
zito-s <zito.s@kakaocorp.com>
2022-02-11 17:48:16 +0900

    feat: 데이터 바인딩

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
-<main>
-  <div class="box" />
-</main>
-
-<style lang="scss">
-  // @import './common.scss'; // common.scss 로드 주석처리
-</style>
-
 <script>
-  console.log('hello')
+	let str1 = '';
+	let str2 = '';
+	let str3 = '';
 </script>
+
+<main>
+	<div>
+		<h3>{str1}</h3>
+		<input type="text" value={str1} /> <!-- 단방향 바인딩 -->
+	</div>
+
+	<div>
+		<h3>{str2}</h3>
+		<input type="text" value={str2} on:change={(e) => str2 = e.target.value} /> <!-- 양방향 바인딩 -->
+	</div>
+
+	<div>
+		<h3>{str3}</h3>
+		<input type="text" bind:value={str3} /> <!-- 양방향 바인딩 -->
+	</div>
+</main>
